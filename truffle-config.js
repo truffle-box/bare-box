5bce5aebb1424985ee76ffd6dcf60fb00267b1d1
cgewecke <christophergewecke@gmail.com>
2018-08-03 15:54:26 -0700

    Unquote network_id

--- truffle-config.js
+++ truffle-config.js
@@ -56,7 +56,7 @@ module.exports = {
     // Another network with more advanced options...
     advanced: {
       // port: 8777,            // Custom port
-      // network_id: "1342",    // Custom network
+      // network_id: 1342,      // Custom network
       // gas: 8500000,          // Gas sent with each transaction (default: ~6700000)
       // gasPrice: 20000000000  // 20 gwei (in wei) (default: 100 gwei)
       // from: <address>        // Account to send txs from (default: accounts[0])
