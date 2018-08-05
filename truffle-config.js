df3ad47e4f19955a17b000df85f46ede02895c11
c-g-e-w-e-k-e- <christophergewecke@gmail.com>
2018-08-05 13:17:57 -0700

    Add missing commas, close braces

--- truffle-config.js
+++ truffle-config.js
@@ -40,7 +40,6 @@ module.exports = {
    */
 
   networks: {
-
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
@@ -55,23 +54,23 @@ module.exports = {
 
     // Another network with more advanced options...
     advanced: {
-      // port: 8777,            // Custom port
-      // network_id: 1342,      // Custom network
-      // gas: 8500000,          // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>        // Account to send txs from (default: accounts[0])
-      // websockets: true       // Enable EventEmitter interface for web3 (default: false)
+      // port: 8777,             // Custom port
+      // network_id: 1342,       // Custom network
+      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+      // from: <address>,        // Account to send txs from (default: accounts[0])
+      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
     },
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     ropsten: {
       // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,      // Ropsten's id
-      // gas: 5500000,       // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,   // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true    // Skip dry run before migrations? (default: false for public nets )
+      // network_id: 3,       // Ropsten's id
+      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
     // Useful for private networks
@@ -90,14 +89,15 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1"    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {         // See the solidity docs for advice about optimization and evmVersion
+      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
       //    enabled: false,
       //    runs: 200
       //  },
       //  evmVersion: "byzantium"
+      // }
     },
 
     external: {
